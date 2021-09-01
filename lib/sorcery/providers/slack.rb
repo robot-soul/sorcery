79854673b3bf9f6ac356c7b72538608a75acb8d0
Simon Bennett <simon@simonbennett.dev>
2021-09-01 23:39:47 +0100

    Add Slack avatar to user hash

--- lib/sorcery/providers/slack.rb
+++ lib/sorcery/providers/slack.rb
@@ -22,6 +22,7 @@ module Sorcery
         auth_hash(access_token).tap do |h|
           h[:user_info] = JSON.parse(response.body)
           h[:user_info]['email'] = h[:user_info]['user']['email']
+          h[:user_info]['slack_avatar'] = h[:user_info]['user']['image_48']
           h[:uid] = h[:user_info]['user']['id']
         end
       end
